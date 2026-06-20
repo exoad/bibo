89b9dcd1996bb094b6e701995b81a5d2338ce36f
jiaming jack meng <45053009+exoad@users.noreply.github.com>
2026-06-20 19:23:14 -0400

    Delete test_ssh.sh

--- test_ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Test SSH connection to remote server
-sshpass -p "Jmeng0814@" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 jack@box.exoad.net "echo 'SSH connection successful'; whoami; pwd"
